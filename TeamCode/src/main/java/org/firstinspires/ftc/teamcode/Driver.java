3713a7cc4b669ff6a75c1873b0647b165e5482a3
Alan Smith <alan@randomsmiths.com>
2023-08-29 18:12:04 -0400

    Added required methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
@@ -3,4 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class Driver extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }

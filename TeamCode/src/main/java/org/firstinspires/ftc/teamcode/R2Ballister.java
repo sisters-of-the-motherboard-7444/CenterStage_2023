de8acbf4ee073f39c9a5ea5b565c378f40ad25df
Jarrett28 <katherine.jarrett@salem.edu>
2023-11-01 15:33:47 -0400

    First Draft of the R2 Ballister Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/R2Ballister.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//REMINDER -- Battery Level effects Motor Power which effects timing
+//this auto program will move a preloaded cone to the substation
+
+@Autonomous (name = "Ballister")
+
+//@Disabled
+
+public class R2Ballister extends LinearOpMode {
+
+    HardwareClassCenterStage Bass_Pro_Shop = new HardwareClassCenterStage();
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+
+        System.out.println("Starting up");
+        telemetry.addData("init pressed", "about to initialize");
+        telemetry.update();
+
+        System.out.println("Initialize Robot");
+        Bass_Pro_Shop.InitializeRobot(hardwareMap);
+        System.out.println("Robot Initialized");
+
+        telemetry.addData("Status", "Ready!");
+
+        telemetry.update();
+
+        waitForStart();
+
+        Bass_Pro_Shop.DriveStraight(.6, 1250, 1); //drive forward to read signal sheet things
+    }
+}

79c8842c175a73d40e7ad0ec73b7108ac05e36f7
Ms. Juran <jeanette.juran@salem.edu>
2024-02-23 18:29:45 -0500

    Gradle updates for OpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV1.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.openftc.easyopencv.OpenCvWebcam;
+
+//REMINDER -- Battery Level effects Motor Power which effects timing
+
+@Autonomous
+
+//@Disabled
+
+public class OpenCV1 extends OpMode {
+
+    OpenCvWebcam webcam
+
+    @Override
+
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

b10bf5b4f1b0962118ed2f491877e6fa191847c5
Ms. Juran <jeanette.juran@salem.edu>
2024-02-23 19:04:29 -0500

    OpenCV code for red object 2 boxes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV1.java
@@ -4,7 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+
 
 //REMINDER -- Battery Level effects Motor Power which effects timing
 
@@ -13,17 +24,83 @@ import org.openftc.easyopencv.OpenCvWebcam;
 //@Disabled
 
 public class OpenCV1 extends OpMode {
-
-    OpenCvWebcam webcam
+    OpenCvWebcam webcam1 = null;
 
     @Override
 
     public void init() {
+        // initial camera for OpenCV - only change camera name (currently webcam1) must match config file on driver's hub
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        // start pipeline code
+        webcam1.setPipeline(new Pipeline1());
+
+        webcam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam1.startStreaming(640,360, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
 
     }
 
     @Override
     public void loop() {
 
+    }
+
+    class Pipeline1 extends OpenCvPipeline{
+        Mat YCbCr = new Mat();
+        Mat leftCrop;
+        Mat rightCrop;
+        double leftavgfin;
+        double rightavgfin;
+        Mat outPut = new Mat();
+        Scalar rectColor = new Scalar(225.0, 0.0, 0.0);
+
+
+        public Mat processFrame(Mat input){
+
+            Imgproc.cvtColor(input, YCbCr,Imgproc.COLOR_RGB2YCrCb);
+            telemetry.addLine("pipeline running");
+
+            Rect leftRect = new Rect(1,1,319,359);
+            Rect rightRect = new Rect(320,1,319,359);
+
+            //code to see bounding boxes on driver's station
+            input.copyTo(outPut);
+            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
+            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
+
+            leftCrop = YCbCr.submat(leftRect);
+            rightCrop = YCbCr.submat(rightRect);
+
+            // extract red color
+            Core.extractChannel(leftCrop, leftCrop, 2);
+            Core.extractChannel(rightCrop, rightCrop, 2);
+
+            // how much red is in each submat
+            Scalar leftavg = Core.mean(leftCrop);
+            Scalar rightavg = Core.mean(rightCrop);
+
+            leftavgfin = leftavg.val[0];
+            rightavgfin = rightavg.val[0];
+
+            if (leftavgfin > rightavgfin){
+                telemetry.addLine("Left");
+            }
+            else {
+                telemetry.addLine("Right");
+            }
+
+            return(outPut);
+        }
     }
 }
